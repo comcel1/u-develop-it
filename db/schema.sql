1ba645354329376f8d18ce843bbffbd0c98f97b7
PaulNelsonOld <90969624+comcel1@users.noreply.github.com>
2022-03-15 14:20:14 -0500

    added table for votes, including adding a delete ability

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
 
 -- using text rather than VARCHAR allows us to enter data that is much longer (like sentences)
 CREATE TABLE parties (
@@ -24,4 +25,20 @@ CREATE TABLE voters (
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+  -- adding constraints to table to prevent voter from voting twice, duplicate records, 
+  -- and what happens if a candidate or voter drops from the database
+  -- uc means voters must be unique
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  -- ON DELETE SET NULL would set the record's field to NULL if the key from the reference table was deleted. 
+  -- With ON DELETE CASCADE, deleting the reference key will also delete the entire row from this table.
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
